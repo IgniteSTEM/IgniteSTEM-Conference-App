c05a3ed96825144f03705f51609e481edd0003d9
Alik Zalmover <alikzalmover@gmail.com>
2019-02-14 16:29:55 -0500

    Uses PDFViewer (test case)

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import PDFReader from "rn-pdf-reader-js";
+import { Constants } from "expo";
 
-export default class App extends Component {
-	render() {
-		return (
-			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-				<Text>Home screen</Text>
-			</View>
-		);
-	}
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <PDFReader
+          source={{
+            uri: "http://gahp.net/wp-content/uploads/2017/09/sample.pdf"
+          }}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1"
+  }
+});
